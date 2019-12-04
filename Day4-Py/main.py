001520d5c577806889ccc7fbacd736bf45a4dbc8
Harvey Frye <harvey.frye@apkudo.com>
2019-12-04 13:26:35 -0600

    Day4 pt 2 in Python done

--- Day4-Py/main.py
+++ Day4-Py/main.py
@@ -1,13 +1,20 @@
 def valid(code):
     double_found = False
     never_decreases = True
+    matches_found = 0
     for i in range(0, len(code) - 1):
         curr = code[i]
         next = code[i+1]
         if curr == next:
-            double_found = True
+            matches_found += 1
+        else:
+            if matches_found == 1:
+                double_found = True
+            matches_found = 0
         if curr > next:
             never_decreases = False
+    if matches_found == 1:
+        double_found = True
     return double_found and never_decreases
 
 def main():
@@ -17,6 +24,7 @@ def main():
     for code in range(range_start, range_end):
         if valid(str(code)):
             count += 1
+    # wrong answer: 944
     print(count)
 
 if __name__ == "__main__":
